be70552e8761f26e5f837a199321f78583897970
Eduardo Visconti <duduhsg@gmail.com>
2023-10-22 15:17:28 -0400

    importing and adding button on html

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,15 @@
 import { Container } from './styles'
 
+import { Button } from '../../components/Button'
+
 export function Details(){
   return (
     <Container>
       <h1>Hello World!</h1>
+      <span>Testttt</span>
+
+      <Button />
+      <Button />
     </Container>
   )
 }
