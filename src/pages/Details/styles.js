2bba339b096581cf4e512686124f94f992d71e27
Eduardo Visconti <duduhsg@gmail.com>
2023-10-22 15:17:58 -0400

    add style to body, input, button, textarea

--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,4 +1,13 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button, textarea {
+    font-style: Robot Slab, serif;
+    font-size: 16px;
+    outline: none;
+  }
 `
