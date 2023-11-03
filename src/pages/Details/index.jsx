4fa1e67cd8717a78b0399057729f1178d4b896a9
Eduardo Visconti <duduhsg@gmail.com>
2023-11-03 09:47:06 -0400

    importing and adding links, content, section, tag and buttontext

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,35 @@
-import { Container } from './styles'
+import { Container, Links, Content } from './styles'
 
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
+import { ButtonText } from '../../components/ButtonText'
 
 export function Details(){
   return (
     <Container>
       <Header />
-      
-      <Button title='Back' />
+
+      <main>
+        <Content>
+          <ButtonText title='Excluir Nota'/>
+        
+          <Section title='Links úteis'>
+            <Links>
+              <li><a href="#">https://www.rocketseat.com.br/</a></li>
+              <li><a href="#">https://www.rocketseat.com.br/</a></li>
+            </Links>
+          </Section>
+
+          <Section title='Marcadores'>
+            <Tag title='express' />
+            <Tag title='nodejs' />
+          </Section>
+        
+          <Button title='Back' />
+        </Content>
+      </main>
     </Container>
   )
 }
